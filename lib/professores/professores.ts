8c4766cb1ccbb8e2dd1841b3c95d990e96b73979
Diogo <diogoferreirasilva16@gmail.com>
2025-03-11 18:22:30 -0300

    .

--- lib/professores/professores.ts
+++ lib/professores/professores.ts
@@ -4,10 +4,23 @@ export async function addProfessor(
   nome: string,
   endereco: string,
   especialidade: string,
-  telefone: string,
+  telefone: number,
   email: string
 ) {
   await pool.query(
-    `insert into professor (nome, endereco, especialidade, telefone, email) values ('${nome}', '${endereco}', '${especialidade}', '${telefone}', '${email}')`
+    `insert into professor
+(nome,
+endereco,
+especialidade,
+telefone,
+email
+) values (
+ $1,
+ $2,
+ $3,
+ $4,
+ $5
+  )`,
+    [nome, endereco, especialidade, telefone, email]
   )
 }
